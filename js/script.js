324afc992f792872fdaddd38b66555748e0e7adb
RakhmanY <yudazrakhman30@gmail.com>
2021-08-11 13:54:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var caribana_main_color = '#c7344b';
+		var caribana_second_color = '#c0ce2b';
+		var caribana_poster = "https://www.caribana-festival.ch/app/themes/caribana_theme/temp_image/caribana-poster.png";
+
+		var live_height = '300';
+		var live_width = '500';
+		var live_autoplay = 'false';
+		var live_player_name = '';
+		var live_embed = 'https://vimeo.com/event/910615/embed';
+
+		var datei18n = {
+			previousMonth : 'Previous Month',
+			nextMonth     : 'Next Month',
+			months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
+			weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+			weekdaysShort : ['Su','Mo','Tu','We','Th','Fr','Sa']
+		};
