f33a6d7bb1a0b4747771c7301eb7855d2e810889
RakhmanY <yudazrakhman30@gmail.com>
2021-08-12 19:27:59 +0700

    navigasi ber-link

---
+++ footer.php
@@ -0,0 +1,148 @@
+<!-- footer -->
+<footer class="footer" role="contentinfo">
+        <div id="footer-container">
+            <div id="footer-row">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/programme/tous/" title="Programme">Juri</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/the-festival/" title="The festival">Bintang Tamu</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/practical/become-a-volunteer/" title="Become a volunteer">Beranda</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/shop/" title="Shop">Tentang Kami</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/stands/" title="Stands">Timeline</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/support-the-festival/partners/" title="Partners">Hadiah</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/contact/" title="Contact">Kontak</a>
+                        </li>
+                        <li>
+                            <a href="https://www.caribana-festival.ch/en/practical/" title="Practical">Daftar</a>
+                        </li>
+                        <!-- <li>
+                            <a href="https://www.caribana-festival.ch/en/support-the-festival/companies-spaces-vip/" title="Corporate VIP Events">Corporate VIP Events</a>
+                        </li> -->
+                    </ul>
+                </nav>					
+                <nav id="social-media">
+                    <ul>
+                        <li>
+                            <a href='https://www.facebook.com/caribana.ch/' target="_blank" class='facebook'></a>
+                        </li>
+                        <li>
+                            <a href='https://twitter.com/caribanafest' target="_blank"  class='twitter'></a>
+                        </li>
+                        <li>
+                            <a href='https://www.instagram.com/caribanafestival/' target="_blank"  class='instagram'></a>
+                        </li>
+                        <li>
+                            <a href='https://www.youtube.com/channel/UCghuZ6zC8D1920BZ3ZpJJzg' target="_blank"  class='youtube'></a>
+                        </li>
+                        <li>
+                            <a href='https://www.flickr.com/photos/caribana-festival/' target="_blank"  class='flickr'></a>
+                        </li>
+                    </ul>
+                    <form class="needs-validation caribana-newsletter"  enctype="multipart/form-data" novalidate>
+                        <div>
+                            <input type='hidden' id='form_name' name='form_name' value='newsletter'>
+                            <div class="form-group"><input id="email" name="email" type="email" class="form-control" required >
+                                <label class="control-label" for="input">Email</label>
+                                    <i class="bar"></i>
+                                <span class="invalid-feedback">Champs incorrect</span>
+                                <span class="icon"></span><span class="info"> &nbsp;</span>
+                            </div>
+                            
+                            <div class="form-group select">
+                                <select class="custom-select" name="lg" required>
+                                    <option  hidden value="" >Language</option>
+                                    <option value="fr"  >French</option>
+                                    <option value="en" selected >English</option>
+                                </select>
+                                <label class="control-label" for="select"></label>
+                                    <i class="bar"></i>
+                                    <span class="info"> &nbsp;</span>
+                            </div>
+                            <div class="form-group">
+                                <input id="zip" name="zip" type="zip" class="form-control" required>
+                                <label class="control-label" for="zip">Zip</label>
+                                    <i class="bar"></i>
+                                <span class="invalid-feedback">Champs incorrect</span>
+                                <span class="icon"></span>
+                                <span class="info"> &nbsp;</span>
+                            </div>
+                        </div>							
+                            <button class="line-effect square dark"></span>
+                                <span class="line">
+                                    <span class="label"></span>
+                                </span>
+                                <span class="btn-bg">
+                            </button>
+                            <div class="form-success">Votre inscription a bien été enregistrée</div>
+                    </form>
+
+                    <div class="copyright desktop">
+                        <a href="http://www.wideagency.ch/" target="_blank">
+                            <span>Website design by wide</span><span class="icon icon-wide"></span>
+                        </a>
+                        <a href="https://www.net4all.ch/" target="_blank">
+                            <span>Hébergé par Net4all</span>
+                            <span class="net4all"></span>
+                        </a>
+                    </div>
+
+                </nav>						
+                <address>
+                    <div id="logo-container-footer">
+                        <img id="desktop-logo" src="asset/logo_caribana_festival_ungu.png" />
+                        <img id="mobile-logo" src="asset/logo_caribana_festival_putih.png" />
+                    </div>
+                    <ul>
+                        <li>Virgin Island NA-7 De Latinons BSD City</li>
+                        <li>Rawabuntu, Serpong - Tangerang Selatan </li>
+                        <li>15318 Indonesia</li>
+                        <li>
+                            <a href='info@griyayatim.com'>info@griyayatim.com</a>
+                        </li>
+                    </ul>
+
+                </address>
+                <div class="copyright mobile">
+                    <a href="http://www.wideagency.ch/" target="_blank"><span>Website design by wide</span><span class="icon icon-wide"></span></a>
+                    <a href="https://www.net4all.ch/" target="_blank"><span>Hébergé par Net4all</span><span class="net4all"></span></a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- /footer -->
+    <script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/manifest.js?ver=1.0.0'></script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/vendor.js?ver=1.0.0'></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var my_ajax_object = {"ajax_url":"https:\/\/www.caribana-festival.ch\/wp\/wp-admin\/admin-ajax.php"};
+    var e_caribana = {"pages":["https:\/\/www.caribana-festival.ch\/en\/e-program\/swiss-made-games-league\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/association-pixel\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/cosplay\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/animations\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/just-dance-geant\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/tournoi-pro\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/concert-lost-frequencies\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/tournoi-amateur\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/faq\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/joueurs\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/e-program\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/description\/"],"is":"false"};
+    /* ]]> */
+    </script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/app.js?ver=1.0.0'></script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/wp/wp-includes/js/wp-embed.min.js?ver=4.9.13'></script>
+
+			<!-- analytics -->
+				<!--script>
+					(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+						(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+						l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+					})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+					ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+					ga('send', 'pageview');
+				</script-->
---
+++ header.php
@@ -0,0 +1,389 @@
+<!doctype html>
+<html lang="en-US" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<title>Festival Anak Muslim</title>
+	
+
+	<!--link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#c7344b">
+	<meta name="theme-color" content="#c7344b"-->
+
+	<link href="//www.google-analytics.com" rel="dns-prefetch">
+	<link href="asset/famicon.svg" rel="shortcut icon">
+	<link href="asset/famicon.svg" rel="apple-touch-icon-precomposed">
+	<!-- <link rel="alternate" type="application/rss+xml" title="Caribana Festival" href="https://www.caribana-festival.ch/en/feed/" /> -->
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Caribana Festival">
+
+	<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+    <link rel='dns-prefetch' href='//www.caribana-festival.ch' />
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link rel='stylesheet' id='oswald-css'  href='https://fonts.googleapis.com/css?family=Oswald%3A200%2C300%7CPoppins%3A400%2C600&#038;ver=4.9.13' media='all' />
+    <link rel='stylesheet' id='caribana-css'  href='css/caribana.css' media='all' />
+    <link rel="stylesheet" id="festival-css" href="css/style.css" media="all">
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <link rel='https://api.w.org/' href='https://www.caribana-festival.ch/wp-json/' />
+    <link rel="alternate" type="application/json+oembed" href="https://www.caribana-festival.ch/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.caribana-festival.ch%2Fen%2F" />
+    <link rel="alternate" type="text/xml+oembed" href="https://www.caribana-festival.ch/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.caribana-festival.ch%2Fen%2F&#038;format=xml" />
+    <meta name="generator" content="qTranslate-X 3.4.6.8" />
+    <link hreflang="x-default" href="https://www.caribana-festival.ch/fr" rel="alternate" />
+    <!-- <link hreflang="" href="https://www.caribana-festival.ch/en" rel="alternate" /> -->
+    <!-- <link hreflang="" href="https://www.caribana-festival.ch/fr" rel="alternate" /> -->
+
+	<script type="application/ld+json">
+        {
+            "@context":"http:\/\/schema.org",
+            "@type":["ItemList","Corporation"],
+            "name":"Site Officiel | Caribana Festival",
+            "description":"Bienvenue au Caribana Festival, le plus grand des petits festivals ! Cette 30e \u00e9dition se d\u00e9roulera du 15 au 19 juin 2022 \u00e0 Crans-pr\u00e8s-Celigny, en Suisse.",
+            "url":"https:\/\/www.caribana-festival.ch\/en\/",
+            "logo":"https:\/\/www.caribana-festival.ch\/app\/uploads\/2018\/09\/logo_caribana_festival_noir.png",
+            "sameAs":["https:\/\/www.facebook.com\/caribana.ch\/",
+            "https:\/\/twitter.com\/caribanafest",
+            "https:\/\/www.instagram.com\/caribanafestival\/",
+            "https:\/\/www.youtube.com\/channel\/UCghuZ6zC8D1920BZ3ZpJJzg",
+            "https:\/\/www.flickr.com\/photos\/caribana-festival\/"],
+            "itemListElement":[]
+        }
+    </script>
+    <meta property="og:url" content="https://www.caribana-festival.ch/en/" />
+    <meta property="og:title" content="Site Officiel | Caribana Festival" />
+    <meta property="og:description" content="Festival Anak Muslim 2021" />
+    <meta property="og:image" content="asset/logo-putih" />
+    <meta property="og:type" content="website" />
+	<script>
+		var caribana_main_color = '#c7344b';
+		var caribana_second_color = '#c0ce2b';
+		var caribana_poster = "https://www.caribana-festival.ch/app/themes/caribana_theme/temp_image/caribana-poster.png";
+
+		var live_height = '300';
+		var live_width = '500';
+		var live_autoplay = 'false';
+		var live_player_name = '';
+		var live_embed = 'https://vimeo.com/event/910615/embed';
+
+		var datei18n = {
+			previousMonth : 'Previous Month',
+			nextMonth     : 'Next Month',
+			months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
+			weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+			weekdaysShort : ['Su','Mo','Tu','We','Th','Fr','Sa']
+		};
+	</script>
+	<!-- Google Tag Manager -->
+	<script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+        {'gtm.start':
+		new Date().getTime(),event:'gtm.js'
+    });
+    
+    var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })
+    (window,document,'script','dataLayer','GTM-NXQQJNZ');
+</script>
+<!-- End Google Tag Manager -->
+
+    <style type="text/css">
+
+#artist-container #artist-shape span
+{
+	background-image:url(asset/blue_Big-1.png);
+}
+
+#caribana-poster #poster #poster-image
+{
+	background-image:url(asset/carib2020_home_blue-1.jpg);
+}
+
+footer .net4all {
+	background-image :url(asset/net-4-all.png);
+}
+@font-face {
+ 	font-family: "home font";
+	 src: url(https://www.caribana-festival.ch/app/uploads/2018/11/timanfaya-webfont.woff2) format("woff2"),
+	 url(https://www.caribana-festival.ch/app/uploads/2018/11/timanfaya-webfont.woff2) format("woff");
+ }
+
+#homepage .font-caribana-home span{
+	color: #000000;
+	font-family: home font !important;
+}
+
+#homepage svg .outer path {
+	fill : #c0ce2b;
+}
+
+#homepage svg .inner path {
+	fill : #642b7c;
+}
+
+#shop-address > div {
+	border-color: #642b7c;
+}
+
+/* FORM ERROR */
+/* validation */
+.form-success-msg,
+form.needs-validation .form-success {
+	color: #4284b6;
+}
+/* error */
+.form-error-msg,
+.TODO {
+	color: #4284b6;
+}
+
+#caribana-days,
+span#dates {
+	background-image : url(asset/festdays-432px.png);
+}
+
+#special-offer .offer-title,
+#artist-infos li a,
+.ticket-day span,
+.line-effect span,
+#other-artists li a,
+.news-slider ul li span,
+#shop-address span,
+#shop-address a
+{
+	color: #642b7c;
+}
+
+#caribana-ad {
+	background-image:url(https://www.caribana-festival.ch/app/themes/caribana_theme/temp_image/wave.svg);
+}
+
+.primary-color,
+#days-navigation a:after,
+#filters-navigation a:after,
+#filters-navigation a:after,
+.ticket-selection span,
+.ticket-selection ul,
+.line-effect span:after,
+.line-effect span:before,
+.line-effect:before,
+.line-effect:after,
+.line-effect .btn-bg,
+.line-effect .rec,
+.pika-single .is-selected .pika-button,
+.pika-single .pika-button:hover,
+#news-container span.flashnews,
+footer
+{
+	background-color: #642b7c;
+}
+
+/* secondary button */
+.secondary.line-effect span {
+	color: #c0ce2b;
+
+}
+.secondary.line-effect span:after,
+.secondary.line-effect span:before,
+.secondary.line-effect:before,
+.secondary.line-effect:after,
+.secondary.line-effect .btn-bg,
+.secondary.line-effect .rec,
+.front-3 {
+	
+	background-color: #c0ce2b;
+}
+
+.primary-color-light:after {	
+
+	background-color : #642b7c;
+}
+
+.news-date ul:after,
+#news-container .news-image:after,
+.img-container:after {
+	background-image : url(asset/Trame_pointillé_bleue-1.png);
+}
+
+body {
+    --main-color: #642b7c;
+    --secondary-color:#c0ce2b;
+}
+
+
+
+</style>
+
+</head>
+<body class="home page-template page-template-pages page-template-homepage page-template-pageshomepage-php page page-id-576 homepage en">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MR5FL89"
+		height="0" width="0" style="display:none;visibility:hidden">
+		</iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<div id="wave-container">
+	<span></span>
+</div>
+
+    <header id="main-header" >
+        <a id="caribana-logo" href="">
+            <img  id="desktop-logo-dark" src="asset/logo_caribana_festival_ungu.png" />
+            <img  id="desktop-logo-light" src="asset/logo-putih.png" />
+            <img  id="mobile-logo-light" src="asset/logo-putih.png" />
+            <img  id="mobile-logo-dark" src="asset/logo_caribana_festival_ungu.png" />
+        </a>
+        <button aria-label="Search" id="search"></button>
+
+        <!-- <ul class="language-chooser language-chooser-custom qtranxs_language_chooser" id="qtranslate-chooser">
+            <li class="language-chooser-item language-chooser-item-en active"><a href="https://www.caribana-festival.ch/en" title="English (en)"><span title="English">en</span></a></li>
+            <li class="language-chooser-item language-chooser-item-fr"><a href="https://www.caribana-festival.ch/fr" title="Français (fr)"><span title="Français">fr</span></a></li>
+        </ul> -->
+        <div class="qtranxs_widget_end">
+            
+        </div>
+            <a id="billetterie-link" class="primary-color-light" href="https://www.caribana-festival.ch/en/ticketing/"><span>Daftar</span></a>
+        <button id="hamburger">
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+
+        <nav>
+            <div id="mobile-slide-container">
+                <div id="navigation-level-1" >
+                    <ul id="primary-navigation" class="menu">
+                            <li id="menu-item-3421" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3421">
+                                <a href="https://www.caribana-festival.ch/en/programme/tous/">Juri & Bintang Tamu</a>
+                            <!-- <a href="https://www.caribana-festival.ch/en/programme/tous/">Programme</a> -->
+                            </li>
+                            <!--
+                            <li id="menu-item-156" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-156">
+                                <a href="https://www.caribana-festival.ch/en/the-festival/">The festival</a>
+                            </li> -->
+                    </ul>				
+                    <ul id="secondary-navigation" class="menu">
+                        <li id="menu-item-2236" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2236">
+                            <a href="https://www.caribana-festival.ch/en/practical/become-a-volunteer/">Tentang Kami</a>
+                        </li>
+                        <li id="menu-item-2358" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2358">
+                            <a href="timeline/">Timeline</a>
+                        </li>
+                        <li id="menu-item-2366" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2366">
+                            <a href="https://www.caribana-festival.ch/en/practical/">Hadiah</a>
+                        </li>
+                        <li id="menu-item-2361" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2361">
+                            <a href="https://www.caribana-festival.ch/en/support-the-festival/companies-spaces-vip/">Hubungi Kami</a>
+                        </li>
+                        <li id="menu-item-3373" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3373">
+                            <a href="https://www.caribana-festival.ch/en/shop/">Daftar</a>
+                        </li>
+                    </ul>					
+                    <ul id="search-mobile">
+                    <li>
+                        <button></button>
+                    </li>
+                    </ul>
+                    <div id="header-social">
+                        <ul>
+                            <li>
+                                <a href='https://www.facebook.com/caribana.ch/' target="_blank" class='facebook'></a>
+                            </li>
+                            <li>
+                                <a href='https://twitter.com/caribanafest' target="_blank"  class='twitter'></a>
+                            </li>
+                            <li>
+                                <a href='https://www.instagram.com/caribanafestival/' target="_blank"  class='instagram'></a>
+                            </li>
+                            <li>
+                                <a href='https://www.youtube.com/channel/UCghuZ6zC8D1920BZ3ZpJJzg' target="_blank"  class='youtube'></a>
+                            </li>
+                            <li>
+                                <a href='https://www.flickr.com/photos/caribana-festival/' target="_blank"  class='flickr'></a>
+                            </li>
+                        </ul>					
+                    </div>
+                </div>
+                <div id="navigation-level-2">
+                    <button class="icon icon-arrow">Retour</button>
+                    <ul data-parent='2366'>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/practical/sleeping/' title='Where to sleep?' class='icon icon-sleep'><span>Where to sleep?</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/practical/reduced-mobility/' title='Reduced mobility' class='icon icon-access'><span>Reduced mobility</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/practical/come/' title='Transport' class='icon icon-marker'><span>Transport</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/practical/items-found/' title='Lost and Found' class='icon icon-question-mark'><span>Lost and Found</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/practical/security-and-prevention/' title='Security and prevention' class='icon icon-security'><span>Security and prevention</span></a>
+                        </li>
+                    </ul>
+                    <ul data-parent='156'>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/news/' title='News' class='icon icon-list'><span>News</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/values/' title='Values' class='icon icon-friends'><span>Values</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/history/' title='History' class='icon icon-timer-1'><span>History</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/stages/' title='Stages' class='icon icon-mic'><span>Stages</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/dj-contest/' title='DJ Contest' class='icon icon-headphone'><span>DJ Contest</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/galery/' title='Galery' class='icon icon-gallery'><span>Galery</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/the-festival/team/' title='Team' class='icon icon-people'><span>Team</span></a>
+                        </li>
+                        <li>
+                            <a href='https://www.caribana-festival.ch/en/stands/' title='Stands' class='icon icon-folder'><span>Stands</span></a>
+                        </li>
+                    </ul>				
+                </div>
+            </div>
+        </nav>
+    </header>
+	
+		
+    
+
+	<script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/manifest.js?ver=1.0.0'></script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/vendor.js?ver=1.0.0'></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var my_ajax_object = {"ajax_url":"https:\/\/www.caribana-festival.ch\/wp\/wp-admin\/admin-ajax.php"};
+    var e_caribana = {"pages":["https:\/\/www.caribana-festival.ch\/en\/e-program\/swiss-made-games-league\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/association-pixel\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/cosplay\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/animations\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/just-dance-geant\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/tournoi-pro\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/concert-lost-frequencies\/","https:\/\/www.caribana-festival.ch\/en\/e-program\/tournoi-amateur\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/faq\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/joueurs\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/e-program\/","https:\/\/www.caribana-festival.ch\/en\/e-caribana\/description\/"],"is":"false"};
+    /* ]]> */
+    </script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/app/themes/caribana_theme/dist/js/app.js?ver=1.0.0'></script>
+    <script type='text/javascript' src='https://www.caribana-festival.ch/wp/wp-includes/js/wp-embed.min.js?ver=4.9.13'></script>
+
+			<!-- analytics -->
+				<!--script>
+					(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+						(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+						l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+					})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+					ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+					ga('send', 'pageview');
+				</script-->
+
+</body>
+</html>
